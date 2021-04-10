3e9807c88f59ecf024386e106898c1277723c6ad
Madeline Hassett <hassettmadeline@gmail.com>
2021-04-09 23:25:30 -0400

    add Drone Fly-Bys

---
+++ javascript/drone-fly-by.js
@@ -0,0 +1,20 @@
+/*
+Drone Fly-By - 7kyu
+
+Description:
+The other day I saw an amazing video where a guy hacked some wifi controlled lightbulbs by flying a drone past them. Brilliant.
+
+In this kata we will recreate that stunt... sort of.
+
+You will be given two strings: lamps and drone. lamps represents a row of lamps, currently off, each represented by x. When these lamps are on, they should be represented by o.
+
+The drone string represents the position of the drone T (any better suggestion for character??) and its flight path up until this point =. The drone always flies left to right, and always begins at the start of the row of lamps. Anywhere the drone has flown, including its current position, will result in the lamp at that position switching on.
+
+Return the resulting lamps string. 
+*/
+
+function flyBy(lamps, drone){
+  let onLamps = 'o'
+  let offLamps = 'x'
+  return `${onLamps.repeat(drone.length)}${offLamps.repeat(lamps.length - drone.length)}`
+}
