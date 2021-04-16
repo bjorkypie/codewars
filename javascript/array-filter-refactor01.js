3cf399cfb905fd841143828f9e9159df818fd685
Madeline Hassett <hassettmadeline@gmail.com>
2021-04-15 23:41:48 -0400

    add refactored Array Filter

---
+++ javascript/array-filter-refactor01.js
@@ -0,0 +1,18 @@
+/*
+Array Filter - 7kyu
+
+Description:
+
+JavaScript Arrays support a filter function (starting in JavaScript 1.6). Use the filter functionality to complete the function given.
+
+The solution would work like the following:
+
+getEvenNumbers([2,4,5,6]) // should == [2,4,6]
+
+Refactored to one line!
+*/
+
+function getEvenNumbers(numbersArray){
+  //return array that filters out odd nums
+  return numbersArray.filter(num => num % 2 === 0 ? true : false)
+}
