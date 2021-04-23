abdd3cff82cea9800920fd4f9e40906d9187ab19
Madeline Hassett <hassettmadeline@gmail.com>
2021-04-22 23:51:38 -0400

    add Convert Bool to String

---
+++ javascript/convert-bool-to-y-n.js
@@ -0,0 +1,14 @@
+/*
+
+Convert Boolean Values to Strings 'Yes' or 'No' - 8kyu
+
+Descripton:
+
+Complete the method that takes a boolean value and return a 'Yes' string for true, or  'No' string for false.
+
+*/
+
+function boolToWord( bool ){
+  //conditional
+  return bool === true ? 'Yes' : 'No';
+}
