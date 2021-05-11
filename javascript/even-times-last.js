7e3f8a27b43d16a9f3ccb4f03c984742f13b9d21
Madeline Hassett <hassettmadeline@gmail.com>
2021-05-11 00:38:44 -0400

    refactor Even Times Last

--- javascript/even-times-last.js
+++ javascript/even-times-last.js
@@ -10,17 +10,13 @@ If the sequence is empty, you should return 0.
 
 */
 
+//refactored shorter
+
 function evenLast(numbers) {
-  //filter even index
-  let evenNums = numbers.filter((num, i) => {
-    return i % 2 === 0 ? true : false;
-  })
-  //reduce to find sum
-  let sum = evenNums.reduce((acc, c) => {
-    return acc + c;
-  }, 0)
-  //return multiplied by last index of input array
-  let result = sum * numbers[numbers.length - 1]
-  //conditional to satisfy when result is NaN
-  return result ? result : 0;
+  //filter even numbers to new array
+  let even = numbers.filter((num, i) => i % 2 === 0);
+  //reduce
+  let evenSum = even.reduce((acc, c) => {return acc + c}, 0);
+  //calculate and return
+  return numbers.length > 0 ? numbers[numbers.length - 1] * evenSum : 0;
 }
