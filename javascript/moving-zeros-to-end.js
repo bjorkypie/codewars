1bd6db6a72c4c61f6995d292b36f9b6e2da4ce19
Madeline Hassett <hassettmadeline@gmail.com>
2021-05-13 23:03:24 -0400

    add Presents and refactor Moving Zeros to End to use splice method

--- javascript/moving-zeros-to-end.js
+++ javascript/moving-zeros-to-end.js
@@ -7,16 +7,15 @@ Write an algorithm that takes an array and moves all of the zeros to the end, pr
 moveZeros([false,1,0,1,2,0,1,3,"a"]) // returns[false,1,1,2,1,3,"a",0,0]
 */
 
-let moveZeros = function (arr) {
-  //new array and filter 0s
-  let endZeros = arr.filter(item => {
-    return item !== 0 ? true : false;
-  })
-  //calc zeros needed
-  let needZeros = arr.length - endZeros.length;
-  //add to end of array
-  for(let i = 0; i < needZeros; i++){
-    endZeros.push(0);
+var moveZeros = function (arr) {
+  //for loop backwards 
+  for(let i = arr.length - 1; i >=0; i--){
+    if(arr[i] === 0){
+      //use splice method to delete
+      arr.splice(i, 1);
+      //add 0 to end
+      arr.push(0);
+    }
   }
-  return endZeros;
+  return arr;
 }
