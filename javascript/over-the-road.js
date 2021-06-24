2c0c39eeec024c90c451dfbfdab6513f9b4d4562
Madeline Hassett <hassettmadeline@gmail.com>
2021-06-24 12:39:37 -0400

    first refactor, math solution vs. memory-heavy array solution

--- javascript/over-the-road.js
+++ javascript/over-the-road.js
@@ -1,25 +1,13 @@
 function overTheRoad(address, n){
     //Evens increase on the right; odds decrease on the left. House numbers start at 1 and increase without gaps. When n = 3, 1 is opposite 6, 3 opposite 4, and 5 opposite 2.
-    let evenArray = []
-    let oddArray = []
-
-    for(let i = (n*2) - 1; i > 0; i -= 2){
-        oddArray.push(i)
-    }
-    for(let j = 2; j <= n*2; j+=2){
-        evenArray.push(j)
-    }
-    console.log(evenArray)
-    console.log(oddArray)
+    let totalhouses = n * 2
+    let steps
     //address is even?
     if(address % 2 === 0){
-        return oddArray[evenArray.indexOf(address)]
-
+    steps = (totalhouses - address)/ 2
+    return 1 + (steps * 2)
     }else if(address % 2 === 1){
-        return evenArray[oddArray.indexOf(address)]
+    steps = (address - 1)/ 2
+    return totalhouses - (2*steps)
     }
-
-    //distance from the first house on the street
-    //even, decreasing by 2 from the last house number
-    //odd, increasing by 2 from 1*/
 }
