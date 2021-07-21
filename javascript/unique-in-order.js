536cf3a432bbc8ac4bf51d59f34a4dfdb632fcf6
Madeline Hassett <hassettmadeline@gmail.com>
2021-07-21 11:35:13 -0400

    add Unique In Order

---
+++ javascript/unique-in-order.js
@@ -0,0 +1,24 @@
+/*
+Unique In Order - 6kyu
+
+Description:
+Implement the function unique_in_order which takes as argument a sequence and returns a list of items without any elements with the same value next to each other and preserving the original order of elements.
+
+For example:
+uniqueInOrder('AAAABBBCCDAABBB') == ['A', 'B', 'C', 'D', 'A', 'B']
+uniqueInOrder('ABBCcAD')         == ['A', 'B', 'C', 'c', 'A', 'D']
+uniqueInOrder([1,2,2,3,3])       == [1,2,3]
+
+Parameters: iterable, string or array
+Results: array of items in order but removing repeats next to one another
+Examples:
+  'ABBCcAD' => ['A', 'B', 'C', 'c', 'A', 'D']
+Pseudocode:
+  check if an array, else split with no delimiter
+  filter array which checks values L -> R
+*/
+
+var uniqueInOrder=function(iterable){
+    let arr = typeof iterable === 'string' ? iterable.split('') : iterable;
+    return arr.filter((el, i, a) => (i === 0) ? true : (el == a[i-1]) ? false : true);
+  }
