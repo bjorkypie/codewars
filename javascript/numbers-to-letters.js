b6d9fc88c370ab7246a05a27f52bf231304e4151
Madeline Hassett <hassettmadeline@gmail.com>
2021-07-24 13:17:13 -0600

    refactored to simplify dictionary from object to string

--- javascript/numbers-to-letters.js
+++ javascript/numbers-to-letters.js
@@ -4,42 +4,23 @@ Numbers to Letters - 7kyu
 Given an array of numbers (in string format), you must return a string. The numbers correspond to the letters of the alphabet in reverse order: a=26, z=1 etc. You should also account for '!', '?' and ' ' that are represented by '27', '28' and '29' respectively.
 
 All inputs will be valid.
-//refactored to use map method
+
+PARAMETERS:
+  array of numbers as strings, which represent: 
+    number in alphabet in reverse order
+    a=26 ... z=1
+    '27', '28', '29' => '!', '?', ' '
+RESULTS:
+  string containing characters which correspond to letter of alphabet in reverse order
+EXAMPLES:
+  ['24', '26', '9', '28'] => 'car?'
+  ['4', '22', '25', '27'] => 'web!'
+PSEUDOCODE:
+  create dictionary object
+  map input to appropriate output
 */
 
 function switcher(x){
-  //declare object with properties assigned to appropriate letters
-  let oppositeAlpha = {
-    26: 'a',
-    25: 'b',
-    24: 'c',
-    23: 'd',
-    22: 'e',
-    21: 'f',
-    20: 'g',
-    19: 'h',
-    18: 'i',
-    17: 'j',
-    16: 'k',
-    15: 'l',
-    14: 'm',
-    13: 'n',
-    12: 'o',
-    11: 'p',
-    10: 'q',
-    9: 'r',
-    8: 's',
-    7: 't',
-    6: 'u',
-    5: 'v',
-    4: 'w',
-    3: 'x',
-    2: 'y',
-    1: 'z',
-    27: '!',
-    28: '?',
-    29: ' ',
-  }
-  //map letters to new array, join and return
-  return x.map(num => oppositeAlpha[num]).join('');
+  const dict = '0zyxwvutsrqponmlkjihgfedcba!? '
+  return x.map(el => dict[Number(el)]).join('')
 }
