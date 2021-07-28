2d4fcca5b890fcd49b236ff88ff634bde55056f2
Madeline Hassett <hassettmadeline@gmail.com>
2021-07-27 22:32:57 -0400

    refactored to separate functionality

--- javascript/vin-checker.js
+++ javascript/vin-checker.js
@@ -57,43 +57,40 @@ PSEUDOCODE:
   return true
   else return false
 */
-
+const dict = {
+'A':1,
+'B':2,
+'C':3, 
+'D':4, 
+'E':5, 
+'F':6, 
+'G':7, 
+'H':8, 
+'J':1, 
+'K':2, 
+'L':3, 
+'M':4, 
+'N':5, 
+'P':7, 
+'R':9, 
+'S':2, 
+'T':3, 
+'U':4, 
+'V':5, 
+'W':6, 
+'X':7, 
+'Y':8, 
+'Z':9 
+}
+const weight = [8,7,6,5,4,3,2,10,0,9,8,7,6,5,4,3,2];
 
 const checkVin = (vin) => {
-    const dict = {
-      'A':1,
-      'B':2,
-      'C':3, 
-      'D':4, 
-      'E':5, 
-      'F':6, 
-      'G':7, 
-      'H':8, 
-      'J':1, 
-      'K':2, 
-      'L':3, 
-      'M':4, 
-      'N':5, 
-      'P':7, 
-      'R':9, 
-      'S':2, 
-      'T':3, 
-      'U':4, 
-      'V':5, 
-      'W':6, 
-      'X':7, 
-      'Y':8, 
-      'Z':9 
-    }
-    const wt = [8,7,6,5,4,3,2,10,0,9,8,7,6,5,4,3,2];
-    if(vin.length != 17 || vin.includes('I') || vin.includes('O') || vin.includes('Q')){
-      return false;
-    }else{
-      let result = vin
-        .split('')
-        .map((el, i) => dict[el] ? dict[el] * wt[i] : Number(el) * wt[i])
-        .reduce(((acc, c) => acc + c), 0) % 11;
-      return (result == vin[8]) ? true : (result === 10 && vin[8] == 'X') ? true : false; 
-    }
-    return false;
-  };
+    return (vin.length != 17) ? false : 
+    (calcModulus(vin) == vin[8]) ?  true : 
+    (calcModulus(vin) === 10 && vin[8] == 'X') ? true : false; 
+}
+
+const calcModulus = (vin) => vin.split('')
+        .map(el => isNaN(el) ? dict[el] : Number(el))
+        .reduce(((acc, c, i) => acc + (c * weight[i])), 0) % 11;
+  
