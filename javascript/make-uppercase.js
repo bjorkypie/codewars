e27a828aa0305dc74c5d20c0733995a6fbfdc1a8
Madeline Hassett <hassettmadeline@gmail.com>
2021-09-08 14:38:56 -0400

    add Make Uppercase

---
+++ javascript/make-uppercase.js
@@ -0,0 +1,16 @@
+/*
+Make Uppercase - 8kyu
+
+Description:
+make a function that returns a string converted to uppercase letters
+
+parameters: string
+results: string made into uppercase
+examples:
+  "what's up doc" => "WHAT'S UP DOC"
+  "watch out" => "WATCH OUT"
+psuedocode:
+  use to uppercase method
+*/
+
+const makeUpperCase = str => str.toUpperCase()
